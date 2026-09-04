8385f28df011e297f29721cb2a05b27d13224f49
Yan_Serra <yankevin054@gmail.com>
2026-09-04 08:50:39 -0300

    Repository de UnidadeFranqueada e Franqueadora criadas

---
+++ src/main/java/com/franquias/gestao/repository/FranqueadoraRepository.java
@@ -0,0 +1,9 @@
+package com.franquias.gestao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.franquias.gestao.model.Franqueadora;
+
+public interface FranqueadoraRepository extends JpaRepository<Franqueadora, Long> {
+
+}
---
+++ src/main/java/com/franquias/gestao/repository/UnidadeFranqueadaRepository.java
@@ -0,0 +1,9 @@
+package com.franquias.gestao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.franquias.gestao.model.UnidadeFranqueada;
+
+public interface UnidadeFranqueadaRepository  extends JpaRepository<UnidadeFranqueada, Long>{
+
+}
